82a9ddee88e0386305b13b93dd5743fb1e6377f4
Ryan James <rjames@rjames-mbpr-it.cdschool.org>
2022-07-25 10:49:39 -0400

    July 18 solution

--- july18/script.js
+++ july18/script.js
@@ -1,24 +1,36 @@
-function hideCharacters(email) {
+function hideCharacters(email, domainFlag) {
     let hiddenEmail = "";
+    let regex = new RegExp("([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\"\(\[\]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\[[\t -Z^-~]*])");
 
     // check if valid email
-    if (email.includes("@")) {
+    if (regex.test(email)) {
 
     // hide characters before @
     let userName = email.split("@")[0];
     userName = "*" + userName.substring(1, userName.length-1) + "*";
     hiddenEmail = userName + "@" + email.split("@")[1];
-    console.log(hiddenEmail);
+    
+    if (domainFlag) {
+        // hide characters after @
+        let domain = email.split("@")[1];
+        let domainArray = domain.split(".");
+        domainArray[0] = "*" + domainArray[0].substring(1, domainArray[0].length-1) + "*";
+        domain = domainArray.join(".");
+        hiddenEmail = hiddenEmail.split("@")[0] + "@" + domain;
+    }
+
+    return hiddenEmail;
 
     } else {
         return email;
     }
 
-    // second flag, hide domain
 }
 
-console.log(hideCharacters("example@example.co.af"));
+console.log(hideCharacters("example@example.co.af.edu", true));
 
 document.getElementById("input").innerHTML += 'example@example.co.af';
 
-document.getElementById("output").innerHTML += '';
+document.getElementById("output").innerHTML += hideCharacters("example@example.co.af.edu", false);
+
+document.getElementById('output-2').innerHTML += hideCharacters("example@example.co.af", true);
