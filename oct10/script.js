5450a274bc9543089436a9f563a273c24fca775d
Ryan James <rjames@rjames-mbpr-it.lan>
2022-10-10 08:18:04 -0400

    oct10 solution complete

---
+++ oct10/script.js
@@ -0,0 +1,39 @@
+// Given two integers, generate a “fibonacci-like” sequence of n digits (where the next number in the pattern is the sum of the previous two numbers).
+
+function truncate(str, n) {
+    // loop over str by character
+    let count = 0;
+    temp = "";
+    let endOfStr = false;
+    for(let i = 0; i < str.length; i++) {
+        // if char is a letter, increment count
+        if (str[i].match(/[a-z]/i) && count < n) {
+            count++;
+            temp += str[i];
+            if (count == n) {
+                endOfStr = true;
+            }
+        }
+        // if char is not a letter
+        else if (!str[i].match(/[a-z]/i)) {
+            temp += str[i];
+            if (endOfStr) {
+                count = 0;
+            }
+        }
+    }
+    return temp;
+}
+
+
+let inputStr = "'*hello* darkness, my ~old_friend'"
+document.getElementById("input").innerHTML += inputStr;
+
+
+let outputStr = ""
+outputStr += truncate(inputStr, 3)
+
+
+document.getElementById("output").innerHTML += outputStr;
+
+
